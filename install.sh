f4d7613fd46582947665804db5959382aa480975
jf <jf@geonef.fr>
2014-06-01 18:55:29 +0200

    comment

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # sysconf install script for conf: nef.common
 
 # update /etc/gitconfig as we just installed files into /etc/gitconfig.d/
+# (nef-update-gitconfig is provided by sysconf.base which is listed our ./deps)
 nef-update-gitconfig
