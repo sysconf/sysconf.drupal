3d9a90a92b936c3c59d4073b8d18980298ff40e3
root <root@gigand>
2014-06-10 23:12:42 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@
 # update /etc/gitconfig as we just installed files into /etc/gitconfig.d/
 # (nef-update-gitconfig is provided by sysconf.base which is listed our ./deps)
 nef-update-gitconfig
+
+######################################################################
+#
+# Same thing with /etc/hosts.d and ~/.ssh/config.d, in case other sysconf profiles change files in them
+nef-update-hosts
+nef-update-ssh-config
+
